7a8c57e864ac93d2e7baf22cc6dd91e8fd9557c3
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2024-06-04 19:12:32 +0900

    Update values.py

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -667,14 +667,14 @@ class CAR(Platforms):
   )
   HYUNDAI_SANTA_FE_2024_MX5 = HyundaiCanFDPlatformConfig(
     [
-      HyundaiCarDocs("Hyundai Santa Fe5 (with HDA II) 2024", "Highway Driving Assist II", car_parts=CarParts.common([CarHarness.hyundai_q])),
+      HyundaiCarDocs("Hyundai Santa Fe (with HDA II) 2024", "Highway Driving Assist II", car_parts=CarParts.common([CarHarness.hyundai_q])),
     ],
     CarSpecs(mass=1915, wheelbase=2.815, steerRatio=14.26, tireStiffnessFactor=0.65),
     flags=HyundaiFlags.ANGLE_CONTROL,
   )
   HYUNDAI_SANTA_FE_2024_HEV_MX5 = HyundaiCanFDPlatformConfig(
     [
-      HyundaiCarDocs("Hyundai Santa Fe5 (with HDA II) 2024", "Highway Driving Assist II", car_parts=CarParts.common([CarHarness.hyundai_q])),
+      HyundaiCarDocs("Hyundai Santa Fe Hybrid (with HDA II) 2024", "Highway Driving Assist II", car_parts=CarParts.common([CarHarness.hyundai_q])),
     ],
     CarSpecs(mass=1930, wheelbase=2.815, steerRatio=14.26, tireStiffnessFactor=0.65),
     flags=HyundaiFlags.HYBRID | HyundaiFlags.ANGLE_CONTROL,
