30b401fd24ed86b20349554d9076737d7b36af4c
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2025-05-16 04:55:13 +0900

    Update params_keys.h
    hda2

--- common/params_keys.h
+++ common/params_keys.h
@@ -320,4 +320,5 @@ inline static std::unordered_map<std::string, uint32_t> keys = {
     {"LFAButtonEngagement", PERSISTENT},
     {"TimeFactorModHours", PERSISTENT},
     {"TimeFactorModMinutes", PERSISTENT},
+    {"CanFdHda2", PERSISTENT},
 };
