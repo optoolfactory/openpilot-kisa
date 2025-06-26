19fb823cefe89a22f4b95f90e8be84a8c5841b89
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2025-06-27 01:31:07 +0900

    Update kisapilot.h

--- selfdrive/ui/qt/widgets/kisapilot.h
+++ selfdrive/ui/qt/widgets/kisapilot.h
@@ -478,6 +478,18 @@ public:
   }
 };
 
+class CanFdHda2Toggle : public ToggleControl {
+  Q_OBJECT
+
+public:
+  CanFdHda2Toggle() : ToggleControl(tr("Set HDA2 Vehicles"), "", "../assets/offroad/icon_shell.png", Params().getBool("CanFdHda2")) {
+    QObject::connect(this, &CanFdHda2Toggle::toggleFlipped, [=](int state) {
+      bool status = state ? true : false;
+      Params().putBool("CanFdHda2", status);
+    });
+  }
+};
+
 class GearDToggle : public ToggleControl {
   Q_OBJECT
 
