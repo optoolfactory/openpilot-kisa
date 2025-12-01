748b309101b37f25b064cc15aed65ab12c7153a8
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2025-12-02 03:43:18 +0900

    Update beep.py

--- selfdrive/controls/beep.py
+++ selfdrive/controls/beep.py
@@ -89,13 +89,10 @@ class Beepd:
         self.dispatch_beep(self.disengage)
       elif new_alert in [AudibleAlert.refuse, AudibleAlert.prompt, AudibleAlert.warningImmediate,AudibleAlert.warningSoft]:
         self.dispatch_beep(self.warning)
-      elif new_alert in [AudibleAlert.bsdWarning, AudibleAlert.laneChange]:
+      elif new_alert in [AudibleAlert.laneChange]:
         self.dispatch_beep(self.ding)
-      elif new_alert in [AudibleAlert.stopStop, AudibleAlert.stopping, AudibleAlert.autoHold, AudibleAlert.engage2, AudibleAlert.disengage2, AudibleAlert.speedDown, AudibleAlert.audioTurn, AudibleAlert.reverseGear]:
+      elif new_alert in [AudibleAlert.stopping, AudibleAlert.autoHold, AudibleAlert.reverseGear]:
         self.dispatch_beep(self.dong)
-      elif new_alert in [AudibleAlert.audio1, AudibleAlert.audio2, AudibleAlert.audio3, AudibleAlert.audio4, AudibleAlert.audio5,
-                         AudibleAlert.audio6, AudibleAlert.audio7, AudibleAlert.audio8, AudibleAlert.audio9, AudibleAlert.audio10]:
-        self.dispatch_beep(self.beep)
 
   def get_audible_alert(self, sm):
     if sm.updated['selfdriveState']:
